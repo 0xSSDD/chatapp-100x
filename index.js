4761801013fcbe6b62abb5121aa8fbea18d0f0c9
SSDD <0xSSDD@protonmail.com>
2023-08-23 17:28:40 +0300

    adds emojisense

--- index.js
+++ index.js
@@ -8,13 +8,36 @@ const io = socketIo(server);
 
 app.use(express.static(__dirname + '/public'));
 
+// Define a mapping of words to emojis
+const wordToEmoji = {
+  react: "⚛️",
+  woah: "😲",
+  hey: "👋",
+  lol: "😂",
+  like: "🤍",
+  congratulations: "🎉",
+};
+
+// Function to replace words with emojis in a message
+function replaceWordsWithEmojis(message) {
+  const words = message.split(" ");
+  const replacedWords = words.map((word) => {
+    if (word.toLowerCase() in wordToEmoji) {
+      return wordToEmoji[word.toLowerCase()] + " ";
+    }
+    return word + " ";
+  });
+  return replacedWords.join("").trim();
+}
+
 io.on('connection', (socket) => {
   console.log('a user connected');
 
   // Listen for messages
   socket.on('chat message', (message) => {
-    io.emit('chat message', message); // Broadcast the message to everyone
-    console.log('message: ' + message);
+    const messageWithEmojis = replaceWordsWithEmojis(message); // Replace words with emojis
+    io.emit('chat message', messageWithEmojis); // Broadcast the message to everyone
+    console.log('message: ' + messageWithEmojis);
   });
 
   // Listen for disconnection
